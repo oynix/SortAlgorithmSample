348df936061e6c3d313b4dfbd761110d25605674
oynix <oynix@foxmail.com>
2021-09-06 10:52:47 +0800

    add insert sort algorithm

--- src/main/kotlin/Sort.kt
+++ src/main/kotlin/Sort.kt
@@ -25,7 +25,6 @@ fun bubble() {
             }
         }
     }
-    println(arr.localString())
 }
 
 // 选择排序
@@ -43,11 +42,68 @@ fun select () {
         }
         arr[pointer++] = temp
     }
-    println(arr.localString())
+}
+
+// 快速排序
+fun quick(low: Int,high: Int, array: IntArray) {
+    if (low >= high) return
+    var left = low
+    var right = high
+    val temp = array[left]
+    var reverse = true
+    while (left < right) {
+        if (reverse) {
+            // 从右指针扫描
+            // 当值比基准值小时，右指针的值填充到左指针，此时认为右指针为空
+            if (array[right] < temp) {
+                array[left] = array[right]
+                reverse = !reverse // 掉头
+            } else {
+                right-- // 右指针左移动
+            }
+        } else {
+            // 从左指针扫描
+            // 当值比基准值大时，左指针填充到右指针，此时认为左指针为空
+            if (array[left] > temp) {
+                array[right] = array[left]
+                reverse = !reverse // 调用
+            } else {
+                left++ // 左指针右移动
+            }
+        }
+    }
+    // 一趟结束后，指针相遇，把基准值放到指针所在位置
+    // 此时左侧都比基准值小，右侧都比基准值大
+    array[left] = temp
+    // 分别递归左侧和右侧
+    quick(low, left - 1, array)
+    quick(left + 1, high, array)
+}
+
+// 插入排序
+fun insert(array: IntArray) {
+    var border = 0 // 左侧已排序部分的边界
+    while (border < array.size - 1) {
+        // 每次取右侧未排序部分的第一个数字，将其插入到左侧的目标位置
+        for (i in border downTo 0) {
+            if (array[i + 1] < array[i]) {
+                // 异或操作交换值，不用额外的临时变量
+                array[i] = array[i] xor array[i + 1]
+                array[i + 1] = array[i] xor array[i + 1]
+                array[i] = array[i] xor array[i + 1]
+            } else {
+                break
+            }
+        }
+        border++
+    }
 }
 
 fun main() {
-//    bubble()
     println(arr.localString())
-    select()
+//    bubble()
+//    select()
+//    quick(0, arr.size - 1, arr)
+    insert(arr)
+    println(arr.localString())
 }
